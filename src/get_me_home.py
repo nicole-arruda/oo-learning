4167309ec52cb2ed91860525cdfd79ad4c32a7fa
Nicole Arruda <nicole.arruda@accenture.com>
2022-08-30 17:06:48 -0400

    Report a random weather event.

--- src/get_me_home.py
+++ src/get_me_home.py
@@ -1,15 +1,22 @@
+from random import randint
+
 from path_finder import PathFinder
 from print_wrapper import PrintWrapper
 
 
 class GetMeHome:
     def __init__(self):
+        self.weather = ["sun", "rain", "snow"]
         self.mode_of_transportation = None
 
     def run(self):
         path_finder = PathFinder(PrintWrapper())
 
         while True:
+            weather = self.weather[randint(0, 2)]
+            print(f"Today's weather: {weather}")
+            print()
+
             print("How do you want to get home?")
             print("  [bike]\n  [car]\n  [metro]\n  [foot]")
             print("  [quit] to stop")
@@ -21,4 +28,5 @@ class GetMeHome:
             self.mode_of_transportation = user_choice
 
             path_finder.best_path_for_travel_by(self.mode_of_transportation)
+            print("=====")
             print()
